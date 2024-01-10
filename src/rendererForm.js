37adda8891e1a7cdbbd54e16eafe1ca6620cdb0e
Nicola Febbrari <febbrari@gmail.com>
2024-01-10 01:33:44 +0100

    form validation

--- src/rendererForm.js
+++ src/rendererForm.js
@@ -5,23 +5,33 @@ document.querySelector('form').addEventListener('submit', function (e) {
 	e.preventDefault(); // Evita il comportamento predefinito dell'invio del modulo
 	const res = {};
 	for (const prop in env) {
-		const value = document.querySelector('#' + prop).value;
-		res[prop] = value;
+		const inputElement = document.getElementById(prop);
+		if (inputElement.type === 'number') {
+			var numberValue = parseInt(inputElement.value);
+			if (isNaN(numberValue)) {
+				alert('Please enter a valid number.');
+				event.preventDefault();
+			} else {
+				res[prop] = numberValue;
+			}
+		} else {
+			res[prop] = inputElement.value;
+		}
 	}
 	ipcRenderer.send('form-submission', res);
 });
 const defaultEnv = {
-	AGE: '50',
+	AGE: 50,
 	APISECRET: '',
-	CARBS_ABS_TIME: '360',
-	CR: '10',
-	DIA: '6',
+	CARBS_ABS_TIME: 360,
+	CR: 10,
+	DIA: 6,
 	GENDER: 'Male',
-	ISF: '30',
+	ISF: 30,
 	LOG_LEVEL: 'info',
 	NIGHTSCOUT_URL: '',
-	TP: '75',
-	WEIGHT: '80',
+	TP: 75,
+	WEIGHT: 80,
 };
 
 // Invia una richiesta asincrona al processo principale
